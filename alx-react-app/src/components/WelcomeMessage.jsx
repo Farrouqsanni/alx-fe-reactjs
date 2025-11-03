089cdf03e83f59e13fee2830cb62bcce20dcb4e8
Farrouqsanni <sannifarrouq@gmail.com>
2025-11-03 14:01:09 +0100

    Final version: WelcomeMessage component working

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,12 +1,10 @@
 function WelcomeMessage() {
-    return (
-        <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
-            <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX!</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+      <p>This is your first React component!</p>
+    </div>
+  );
 }
 
 export default WelcomeMessage;
-
